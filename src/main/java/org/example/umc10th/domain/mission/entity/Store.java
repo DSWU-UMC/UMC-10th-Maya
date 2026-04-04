dab47b14b48a2800168d6fdaf59761b71a199f5e
wooarling <gripissmin1@gmail.com>
2026-04-04 12:27:20 +0900

    FEAT: 4주차 도메인 아키텍쳐 설계

--- src/main/java/org/example/umc10th/domain/mission/entity/Store.java
+++ src/main/java/org/example/umc10th/domain/mission/entity/Store.java
@@ -1,4 +1,12 @@
 package org.example.umc10th.domain.mission.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Store {
+    @Id
+    private Long id;
 }
